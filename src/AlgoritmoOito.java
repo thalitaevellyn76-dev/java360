aca039685d753beb01bf9ecfde9c1e46ffe65f0b
java360 <thalitaevellyn76@gmail.com>
2026-09-04 11:06:48 -0300

    Algoritmo 8: Produto de dois números

--- src/AlgoritmoOito.java
+++ src/AlgoritmoOito.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class AlgoritmoOito {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("--- Calculadora de Produto ---");
+        System.out.print("Digite o primeiro número: ");
+        int num1 = entrada.nextInt();
+        
+        System.out.print("Digite o segundo número: ");
+        int num2 = entrada.nextInt();
+        
+        int produto = num1 * num2;
+        
+        System.out.println("Produto: " + produto);
+        
+        entrada.close();
+    }
 }
