d177b95fb9b7d4b435eabbd6df5d2377f5ed0b7d
java360 <thalitaevellyn76@gmail.com>
2026-09-04 11:12:10 -0300

    Algoritmo 9: Cálculo da terça parte de um número real

--- src/AlgoritmoNove.java
+++ src/AlgoritmoNove.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class AlgoritmoNove {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("--- Cálculo da Terça Parte ---");
+        System.out.print("Digite um número real: ");
+        double numero = entrada.nextDouble();
+        
+        double tercaParte = numero / 3.0;
+        
+        System.out.println("A terça parte é: " + tercaParte);
+        
+        entrada.close();
+    }
 }
