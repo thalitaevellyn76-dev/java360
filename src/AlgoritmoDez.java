4c8ed877b9b16d28187e73c5d29da832f685b6c3
java360 <thalitaevellyn76@gmail.com>
2026-09-04 11:38:21 -0300

    Algoritmo 10: Média aritmética de dois números

--- src/AlgoritmoDez.java
+++ src/AlgoritmoDez.java
@@ -1,3 +1,24 @@
-public class AlgoritmoDez {
+ import java.util.Scanner;
+ 
+ public class AlgoritmoDez {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("--- Calculo da Media Aritmetica ---");
+        System.out.println("Digite o priemiro numero real: ");
+        double num1 = entrada.nextDouble();
+
+        System.out.println("Digite o primeiro numero real: ");
+        double num2 = entrada.nextDouble();
+
+        double media = (num1 + num2) / 2.0;
+
+        System.out.println("media: " + media);
+
+        entrada.close();
+
+
+
+    }
     
 }
