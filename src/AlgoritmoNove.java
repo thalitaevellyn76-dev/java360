0ce8382dc1e93ae224e6ba9c0c84a1a20df578a7
java360 <thalitaevellyn76@gmail.com>
2026-09-08 11:21:24 -0300

    feat: atualiza algoritmos do projeto Java

--- src/AlgoritmoNove.java
+++ src/AlgoritmoNove.java
@@ -4,13 +4,16 @@ public class AlgoritmoNove {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("--- Cálculo da Terça Parte ---");
-        System.out.print("Digite um número real: ");
-        double numero = entrada.nextDouble();
+        System.out.println("--- Calculadora de Produto ---");
+        System.out.print("Digite o primeiro número: ");
+        int num1 = entrada.nextInt();
         
-        double tercaParte = numero / 3.0;
+        System.out.print("Digite o segundo número: ");
+        int num2 = entrada.nextInt();
         
-        System.out.println("A terça parte é: " + tercaParte);
+        int produto = num1 * num2;
+        
+        System.out.println("Produto: " + produto);
         
         entrada.close();
     }
--- src/AlgoritmoOito.java
+++ src/AlgoritmoOito.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class AlgoritmoOito {
+public class AlgoritmoOito{
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("--- Calculadora de Produto ---");
+        System.out.println("--- Calculadora de Soma ---");
         System.out.print("Digite o primeiro número: ");
         int num1 = entrada.nextInt();
         
         System.out.print("Digite o segundo número: ");
         int num2 = entrada.nextInt();
         
-        int produto = num1 * num2;
+        int soma = num1 + num2;
         
-        System.out.println("Produto: " + produto);
+        System.out.println("Soma: " + soma);
         
         entrada.close();
     }
