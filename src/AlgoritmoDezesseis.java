a8aae083a309b2dcf97e531372383bde929ad1f9
java360 <thalitaevellyn76@gmail.com>
2026-09-09 11:23:30 -0300

    Adiciona exercicio AlgoritmoDezesseis

---
+++ src/AlgoritmoDezesseis.java
@@ -0,0 +1,10 @@
+public class AlgoritmoDezesseis {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 200) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
