14ac9bfc7671170058374a69589810dc5ca6ee8c
java360 <thalitaevellyn76@gmail.com>
2026-09-10 09:20:25 -0300

    Ajusta exibicao da media no AlgoritmoVinteUM

--- src/AlgoritmoVinteUM.java
+++ src/AlgoritmoVinteUM.java
@@ -1,17 +1,16 @@
 public class AlgoritmoVinteUM {
     public static void main(String[] args) {
-        int[] numeros = {45 , 67 , 89 , 34 , 23 , 56 , 78 , 45 ,98 , 34 };
+        int[] numeros = {45, 67, 89, 34, 23, 56, 78, 45, 98, 34};
 
         double soma = 0;
-        
-        for(int i = 0; i < numeros.length; i++){
+
+        for(int i = 0; i < numeros.length; i++) {
             soma += numeros[i];
         }
-        
+
         double media = soma / numeros.length;
 
         System.out.println("Soma total: " + (int) soma);
         System.out.println("Média: " + media);
     }
-    
 }
