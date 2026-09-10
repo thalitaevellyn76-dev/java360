05a79f7a2da41a7e01d105619697a9eadada1fbb
java360 <thalitaevellyn76@gmail.com>
2026-09-10 08:55:20 -0300

    Corrige sintaxe e implementa AlgoritmoVinteUm

---
+++ src/AlgoritmoVinteUM.java
@@ -0,0 +1,12 @@
+public class AlgoritmoVinteUM {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {45, 67, 89, 34, 56, 78, 98, 45, 34};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
