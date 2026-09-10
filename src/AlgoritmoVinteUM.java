083c3974851eeb6fbef31ca316c20c117933ac2c
java360 <thalitaevellyn76@gmail.com>
2026-09-10 09:08:59 -0300

    Adiciona exercicio AlgoritmoVinteUM

--- src/AlgoritmoVinteUM.java
+++ src/AlgoritmoVinteUM.java
@@ -1,12 +1,17 @@
 public class AlgoritmoVinteUM {
-
     public static void main(String[] args) {
+        int[] numeros = {45 , 67 , 89 , 34 , 23 , 56 , 78 , 45 ,98 , 34 };
 
-        int[] numeros = {45, 67, 89, 34, 56, 78, 98, 45, 34};
-
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println(numeros[i]);
+        double soma = 0;
+        
+        for(int i = 0; i < numeros.length; i++){
+            soma += numeros[i];
         }
+        
+        double media = soma / numeros.length;
 
+        System.out.println("Soma total: " + (int) soma);
+        System.out.println("Soma total: " + media );
     }
+    
 }
