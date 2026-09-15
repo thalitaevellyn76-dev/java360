e03b293b4749d1e273829c897e8d07134853eaab
java360 <thalitaevellyn76@gmail.com>
2026-09-15 11:48:57 -0300

    Adiciona Algoritmo31.java

---
+++ javaprojetos/src/Algoritmo31.java
@@ -0,0 +1,18 @@
+public class Algoritmo31 {
+     //camelCasing
+    /*<<modificador <<tipo>> nomeDoMetodo(parametros){
+    
+    }
+    */
+
+   public String getAloMundo(){
+
+        return "Hello World GET!";
+   }
+
+   public void printarNaTela(){
+
+     IO.print( "Alo Mundo! Esse Printa!");
+   }
+   
+}
