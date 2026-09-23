15299ec4d6c9f592216e2d032b87650c4604be8f
java360 <thalitaevellyn76@gmail.com>
2026-09-23 09:14:22 -0300

    Adiciona Algoritmo36

--- src/Algoritmo40.java
+++ src/Algoritmo40.java
@@ -8,7 +8,14 @@ public class Algoritmo40 {
        //autoCA, revit, sketchup
        //humanoide - softwares 3D simulacao
        //Minecraft x, y e z
-         */
+      */
+     //vetor ou matriz unidimensional
+     //https://www.somamatematica.com.br/emedio/matrizes/matriz
+     //matriz linha ou matriz coluna
+     //              0 1 2 3 4
+     int[] notas = {7,9,5,10,6};
+     int maior = notas[0];
+     IO.println(maior);
      }
 
 
