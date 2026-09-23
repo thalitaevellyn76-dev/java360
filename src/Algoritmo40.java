d811a67fceec5f21dcd00f3514cfc17c82f07afb
java360 <thalitaevellyn76@gmail.com>
2026-09-23 09:07:20 -0300

    Adiciona Algoritmo35

--- src/Algoritmo40.java
+++ src/Algoritmo40.java
@@ -1,5 +1,15 @@
 public class Algoritmo40 {
-    
+     public void main(){
+        //vetor - matriz unidimensional
+        /*
+        tabela - matriz bidimensional
+        30 - matriz tridimensional
+       //cinema, desenhos, animacoes,games (gta 6) 
+       //autoCA, revit, sketchup
+       //humanoide - softwares 3D simulacao
+       //Minecraft x, y e z
+         */
+     }
 
 
 
